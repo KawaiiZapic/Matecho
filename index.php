532bbc9ed0ecc7bd99c0ec5970b046de53593350
Zapic <zapic233@gmail.com>
2019-11-18 14:34:35 +0800

    Add archive info & more

--- index.php
+++ index.php
@@ -12,24 +12,57 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
  $this->need('header.php');
 ?>
 <div id="content-box" class="mdui-container">
-<?php while($this->next()): ?>
+	<?php while($this->next()): ?>
 	<div class="mdui-card card-archive mdui-center">
-  <div class="mdui-card-media">
-<div style="background:url(<?php $this->options->themeUrl("static/img/material-1.png");?>);height:300px;background-position:center center;background-size:cover"> </div>
-    <div class="mdui-card-media-covered">
-      <div class="mdui-card-primary">
-        <div class="mdui-card-primary-title" ><?php $this->title() ?></div>
-        <div class="mdui-card-primary-subtitle mdui-text-truncate"><?php $this->excerpt(100, '...'); ?></div>
-      </div>
-    </div>
-  </div>
-  <div class="mdui-card-actions">
-	<a href="<?php $this->permalink(); ?>" class="mdui-btn mdui-ripple mdui-float-right mdui-color-theme-accent">开始阅读</a>
-	Published by <a href="<?php $this->author->permalink(); ?>"><?php $this->author(); ?></a> on <?php $this->date('F j, Y'); ?> in <?php $this->category(','); ?>.
-	    <?php $this->commentsNum('%d Comments'); ?>.
-  </div>
+		<div class="mdui-card-media">
+			<div
+				style="background:url(<?php $this->options->themeUrl("static/img/material-1.png");?>);height:300px;background-position:center center;background-size:cover">
+			</div>
+			<div class="mdui-card-media-covered">
+				<div class="mdui-card-primary">
+					<div class="mdui-card-primary-title"><?php $this->title() ?></div>
+					<div class="mdui-card-primary-subtitle mdui-text-truncate"><?php $this->excerpt(100, '...'); ?>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div class="mdui-card-actions">
+			<div class="mdui-chip" onclick="window.location='<?php $this->author->permalink(); ?>'">
+				<img class="mdui-chip-icon" src="<?php echo getGravatar($this->author->mail);?>" />
+				<span class="mdui-chip-title"><?php $this->author->screenName(); ?></span>
+			</div>
+			<div class="mdui-chip"
+				onclick="window.location='<?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')->parse("{permalink}"); ?>'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i
+						class="mdui-icon material-icons">today</i></span>
+				<span class="mdui-chip-title"><?php $this->date(); ?> </span>
+			</div>
+			<div class="mdui-chip mdui-hidden-sm-down"
+				onclick="window.location='<?php $this->widget('Widget_Metas_Category_List')->parse("{permalink}"); ?>'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i class="mdui-icon material-icons">apps</i></span>
+				<span class="mdui-chip-title"><?php $this->widget('Widget_Metas_Category_List')->name(); ?></span>
+			</div>
+			<div class="mdui-chip mdui-hidden-sm-down" <?php if(count($this->tags)>0){ ?>mdui-menu="{target:'#tagmenu_<?php echo $this->cid(); ?>',position:'top',covered:false}"<?php } ?>>
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i class="mdui-icon material-icons">local_offer</i></span>
+				<span class="mdui-chip-title"><?php echo count($this->tags)>0?count($this->tags)."个":"无";?>标签</span>
+			</div>
+			<?php if (count($this->tags)>0){ ?>
+			<ul class="mdui-menu" id="tagmenu_<?php echo $this->cid(); ?>">
+				<li class="mdui-menu-item mdui-ripple">
+					<?php $this->tags('<li class="mdui-menu-item mdui-ripple">',true,''); ?></li>
+			</ul>
+			<?php } ?>
+			<div class="mdui-chip mdui-hidden-sm-down"
+				onclick="window.location='<?php $this->permalink(); ?>#comments'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i
+						class="mdui-icon material-icons">comment</i></span>
+				<span class="mdui-chip-title"><?php $this->commentsNum('0 条评论', '1 条评论', '%d 条评论'); ?></span>
+			</div>
+			<a href="<?php $this->permalink(); ?>" class="mdui-btn mdui-ripple mdui-float-right mdui-color-theme-accent">开始阅读</a>
+		</div>
+	</div>
+	<?php endwhile; ?>
 </div>
-<?php endwhile; ?>
-</div>
+<?php if($this->is('index')){$this->pageNav('前一页','后一页',1,'...',array('wrapTag' => 'div','wrapClass' => 'page-navigator mdui-card mdui-p-a-1 mdui-m-y-2 mdui-text-center'));} ?>
 
 <?php $this->need('footer.php');?>
--- post.php
+++ post.php
@@ -15,13 +15,38 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 				</div>
 			</div>
 		</div>
-		<div class="mdui-card-actions" onclick="window.loaction='<?php $this->author->permalink(); ?>'">
-		<div class="mdui-chip">
-  			<img class="mdui-chip-icon" src="<?php echo getGravatar($this->author->mail);?>"/>
-  			<span class="mdui-chip-title mdui-text-color-theme-accent"><?php $this->author->screenName(); ?></span>
-		</div>
-		<?php $this->date(); ?> in <?php $this->category(','); ?>.
-	    	<?php $this->commentsNum('%d Comments'); ?>.
+		<div class="mdui-card-actions">
+			<div class="mdui-chip" onclick="window.location='<?php $this->author->permalink(); ?>'">
+				<img class="mdui-chip-icon" src="<?php echo getGravatar($this->author->mail);?>" />
+				<span class="mdui-chip-title"><?php $this->author->screenName(); ?></span>
+			</div>
+			<div class="mdui-chip"
+				onclick="window.location='<?php $this->widget('Widget_Contents_Post_Date', 'type=month&format=F Y')->parse("{permalink}"); ?>'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i
+						class="mdui-icon material-icons">today</i></span>
+				<span class="mdui-chip-title"><?php $this->date(); ?> </span>
+			</div>
+			<div class="mdui-chip mdui-hidden-sm-down"
+				onclick="window.location='<?php $this->widget('Widget_Metas_Category_List')->parse("{permalink}"); ?>'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i class="mdui-icon material-icons">apps</i></span>
+				<span class="mdui-chip-title"><?php $this->widget('Widget_Metas_Category_List')->name(); ?></span>
+			</div>
+			<div class="mdui-chip mdui-hidden-sm-down" <?php if(count($this->tags)>0){ ?>mdui-menu="{target:'#tagmenu_<?php echo $this->cid(); ?>',position:'top',covered:false}"<?php } ?>>
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i class="mdui-icon material-icons">local_offer</i></span>
+				<span class="mdui-chip-title"><?php echo count($this->tags)>0?count($this->tags)."个":"无";?>标签</span>
+			</div>
+			<?php if (count($this->tags)>0){ ?>
+			<ul class="mdui-menu" id="tagmenu_<?php echo $this->cid(); ?>">
+				<li class="mdui-menu-item mdui-ripple">
+					<?php $this->tags('<li class="mdui-menu-item mdui-ripple">',true,''); ?></li>
+			</ul>
+			<?php } ?>
+			<div class="mdui-chip mdui-hidden-sm-down"
+				onclick="window.location='<?php $this->permalink(); ?>#comments'">
+				<span class="mdui-chip-icon mdui-color-theme-accent"><i
+						class="mdui-icon material-icons">comment</i></span>
+				<span class="mdui-chip-title"><?php $this->commentsNum('0 条评论', '1 条评论', '%d 条评论'); ?></span>
+			</div>
 		<div class="mdui-typo"><hr></div>
 			
   		</div>
@@ -33,5 +58,5 @@ if (!defined('__TYPECHO_ROOT_DIR__')) exit;
 	</div>
 	<?php endwhile; ?>
 </div>
-
+<?php include('comments.php'); ?>
 <?php $this->need('footer.php');?>
