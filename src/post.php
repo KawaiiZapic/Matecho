5d95d722df4e6eac0b93df731f3a7f36feba6c91
KawaiiZapic <kawaiizapic@zapic.moe>
2026-05-20 16:54:03 +0800

    fix: 在暗色模式也为文章内卡片添加背景色

--- src/post.php
+++ src/post.php
@@ -25,7 +25,7 @@ $this->need("header.php");
                 } ?>
             </div>
         </div>
-        <div class="matecho-article-cover mb-8 md:rounded-xl transition block h-240px w-full overflow-hidden bg-center bg-cover bg-m-surface-container-low"
+        <div class="matecho-article-cover mb-8 md:rounded-xl transition block h-240px w-full overflow-hidden bg-center bg-cover"
             style="background-image: url('<?php Matecho::cover($this); ?>')">
         </div>
         <div class="w-full px-4 box-border">
